f8f9a2f5e898f57f9c8d00beee2b5134be67cc24
Ehtesh Choudhury <ehtesh.choudhury@gmail.com>
2015-05-19 09:32:20 -0400

    refactor Helpers into its own class

--- src/test/java/nyc/c4q/Unit1AssessmentTests.java
+++ src/test/java/nyc/c4q/Unit1AssessmentTests.java
@@ -1,7 +1,5 @@
 package nyc.c4q;
 
-import android.app.Activity;
-import android.content.res.Resources;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -24,26 +22,6 @@ import static org.junit.Assert.assertTrue;
 @Config(manifest = "src/main/AndroidManifest.xml", emulateSdk = 18)
 public class Unit1AssessmentTests {
 
-    // ===================================== HELPERS START
-
-    public static String getResourceId(View v){
-        // http://stackoverflow.com/a/17583380/198348
-        int id = v.getId();
-        String idString = "no id";
-        if(id != View.NO_ID) {
-            Resources res = v.getResources();
-            if(res != null)
-                idString = res.getResourceEntryName(id);
-        }
-        return idString;
-    }
-
-    public static View findViewByIdString(Activity a, String s){
-        return a.findViewById(a.getResources().getIdentifier(s, "id", a.getPackageName()));
-    }
-
-    // ===================================== HELPERS END
-
     @Test
     public void test01FixInitialActivityLayout() throws Exception {
         InitialActivity activity = Robolectric.buildActivity(InitialActivity.class).create().get();
@@ -52,46 +30,46 @@ public class Unit1AssessmentTests {
         // TODO only need the last assertTrue() method per view
         assertTrue("LinearLayout(@+id/activity_initial)[0] should be a LinearLayout", layout.getChildAt(0) instanceof LinearLayout);
         LinearLayout counterLayout = (LinearLayout) layout.getChildAt(0);
-        assertTrue("LinearLayout(@+id/activity_initial)[0] should have R.id.counterLayout", getResourceId(counterLayout).equals("counterLayout"));
-        assertTrue("LinearLayout(@+id/activity_initial)[0] should be equal to LinearLayout(@+id/counterLayout)", counterLayout == findViewByIdString(activity, "counterLayout"));
+        assertTrue("LinearLayout(@+id/activity_initial)[0] should have R.id.counterLayout", Helpers.getResourceId(counterLayout).equals("counterLayout"));
+        assertTrue("LinearLayout(@+id/activity_initial)[0] should be equal to LinearLayout(@+id/counterLayout)", counterLayout == Helpers.findViewByIdString(activity, "counterLayout"));
         assertTrue("LinearLayout(@+id/activity_initial)[0] should have a horizontal orientation", counterLayout.getOrientation() == LinearLayout.HORIZONTAL);
 
         assertTrue("LinearLayout(@+id/counterLayout)[0] should be a LinearLayout", counterLayout.getChildAt(0) instanceof LinearLayout);
         LinearLayout counterButtonsLayout = (LinearLayout) counterLayout.getChildAt(0);
-        assertTrue("LinearLayout(@+id/counterLayout)[0] should have R.id.counterButtonsLayout", getResourceId(counterButtonsLayout).equals("counterButtonsLayout"));
-        assertTrue("LinearLayout(@+id/counterLayout)[0] should be equal to LinearLayout(@+id/counterButtonsLayout)", counterButtonsLayout == findViewByIdString(activity, "counterButtonsLayout"));
+        assertTrue("LinearLayout(@+id/counterLayout)[0] should have R.id.counterButtonsLayout", Helpers.getResourceId(counterButtonsLayout).equals("counterButtonsLayout"));
+        assertTrue("LinearLayout(@+id/counterLayout)[0] should be equal to LinearLayout(@+id/counterButtonsLayout)", counterButtonsLayout == Helpers.findViewByIdString(activity, "counterButtonsLayout"));
         assertTrue("LinearLayout(@+id/counterLayout)[0] should have a vertical orientation", counterButtonsLayout.getOrientation() == LinearLayout.VERTICAL);
 
         assertTrue("LinearLayout(@+id/counterButtonsLayout)[0] should be a Button", counterButtonsLayout.getChildAt(0) instanceof Button);
         Button buttonPlus = (Button) counterButtonsLayout.getChildAt(0);
-        assertTrue("LinearLayout(@+id/counterButtonsLayout)[0] should have R.id.buttonPlus", getResourceId(buttonPlus).equals("buttonPlus"));
-        assertTrue("LinearLayout(@+id/counterButtonsLayout)[0] should be equal to Button(@+id/buttonPlus)", buttonPlus == findViewByIdString(activity, "buttonPlus"));
+        assertTrue("LinearLayout(@+id/counterButtonsLayout)[0] should have R.id.buttonPlus", Helpers.getResourceId(buttonPlus).equals("buttonPlus"));
+        assertTrue("LinearLayout(@+id/counterButtonsLayout)[0] should be equal to Button(@+id/buttonPlus)", buttonPlus == Helpers.findViewByIdString(activity, "buttonPlus"));
         assertEquals("+", buttonPlus.getText());
 
         assertTrue("LinearLayout(@+id/counterButtonsLayout)[1] should be a Button", counterButtonsLayout.getChildAt(1) instanceof Button);
         Button buttonMinus = (Button) counterButtonsLayout.getChildAt(1);
-        assertTrue("LinearLayout(@+id/counterButtonsLayout)[1] should have R.id.buttonMinus", getResourceId(buttonMinus).equals("buttonMinus"));
-        assertTrue("LinearLayout(@+id/counterButtonsLayout)[1] should be equal to Button(@+id/buttonMinus)", buttonMinus == findViewByIdString(activity, "buttonMinus"));
+        assertTrue("LinearLayout(@+id/counterButtonsLayout)[1] should have R.id.buttonMinus", Helpers.getResourceId(buttonMinus).equals("buttonMinus"));
+        assertTrue("LinearLayout(@+id/counterButtonsLayout)[1] should be equal to Button(@+id/buttonMinus)", buttonMinus == Helpers.findViewByIdString(activity, "buttonMinus"));
         assertEquals("-", buttonMinus.getText());
 
         View tvCounter = counterLayout.getChildAt(1);
         assertTrue("LinearLayout(@+id/activity_initial)[0][1] should be equal to TextView(@+id/tvCounter)",
-                tvCounter instanceof TextView && getResourceId(tvCounter).equals("tvCounter"));
+                tvCounter instanceof TextView && Helpers.getResourceId(tvCounter).equals("tvCounter"));
         assertEquals("0", ((TextView) tvCounter).getText());
 
         View buttonTileActivity = layout.getChildAt(1);
         assertTrue("LinearLayout(@+id/activity_initial)[1] should be equal to Button(@+id/buttonTileActivity)",
-                buttonTileActivity instanceof Button && getResourceId(buttonTileActivity).equals("buttonTileActivity"));
+                buttonTileActivity instanceof Button && Helpers.getResourceId(buttonTileActivity).equals("buttonTileActivity"));
         assertEquals("TileActivity", ((Button) buttonTileActivity).getText());
 
         View buttonNewsReaderActivity = layout.getChildAt(2);
         assertTrue("LinearLayout(@+id/activity_initial)[1] should be equal to Button(@+id/buttonNewsReaderActivity)",
-                buttonNewsReaderActivity instanceof Button && getResourceId(buttonNewsReaderActivity).equals("buttonNewsReaderActivity"));
+                buttonNewsReaderActivity instanceof Button && Helpers.getResourceId(buttonNewsReaderActivity).equals("buttonNewsReaderActivity"));
         assertEquals("NewsReaderActivity", ((Button) buttonNewsReaderActivity).getText());
 
         View buttonBigTextActivity = layout.getChildAt(3);
         assertTrue("LinearLayout(@+id/activity_initial)[1] should be equal to Button(@+id/buttonBigTextActivity)",
-                buttonBigTextActivity instanceof Button && getResourceId(buttonBigTextActivity).equals("buttonBigTextActivity"));
+                buttonBigTextActivity instanceof Button && Helpers.getResourceId(buttonBigTextActivity).equals("buttonBigTextActivity"));
         assertEquals("BigTextActivity", ((Button) buttonBigTextActivity).getText());
 
         assertEquals("LinearLayout(@+id/counterLayout) should have layout_weight='2'", 2, ((LinearLayout.LayoutParams) counterLayout.getLayoutParams()).weight, 0.01);
@@ -107,7 +85,7 @@ public class Unit1AssessmentTests {
         assertEquals("LinearLayout(@+id/counterLayout) should have layout_height='0dp'", 0, ((LinearLayout.LayoutParams) counterLayout.getLayoutParams()).height);
         assertEquals("Button      (@+id/buttonTileActivity) should have layout_height='0dp'", 0, ((LinearLayout.LayoutParams) buttonTileActivity.getLayoutParams()).height);
         assertEquals("Button      (@+id/buttonNewsReaderActivity) should have layout_height='0dp'", 0, ((LinearLayout.LayoutParams) buttonNewsReaderActivity.getLayoutParams()).height);
-        assertEquals("Button      (@+ilayout_weightd/buttonBigTextActivity) should have layout_height='0dp'", 0, ((LinearLayout.LayoutParams) buttonBigTextActivity.getLayoutParams()).height);
+        assertEquals("Button      (@+i/buttonBigTextActivity) should have layout_height='0dp'", 0, ((LinearLayout.LayoutParams) buttonBigTextActivity.getLayoutParams()).height);
         assertEquals("LinearLayout(@+id/counterButtonsLayout) should have layout_width='0dp'", 0, ((LinearLayout.LayoutParams) counterButtonsLayout.getLayoutParams()).width);
         assertEquals("TextView    (@+id/tvCounter) should have layout_width='0dp'", 0, ((LinearLayout.LayoutParams) tvCounter.getLayoutParams()).width);
         assertEquals("Button      (@+id/buttonPlus) should have layout_height='0dp'", 0, ((LinearLayout.LayoutParams) buttonPlus.getLayoutParams()).height);
