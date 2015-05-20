5f1a70a5fadb5c1474aee3fcbe04b38b8d6b1086
personabe1984 <personabe1984@gmail.com>
2015-05-19 20:43:00 -0400

    half of the test

--- src/main/java/nyc/c4q/Unit1AssessmentApplication.java
+++ src/main/java/nyc/c4q/Unit1AssessmentApplication.java
@@ -1,7 +1,11 @@
 package nyc.c4q;
 
 import android.app.Application;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class Unit1AssessmentApplication extends Application {
+
 
 }
