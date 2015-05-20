1097a49540d8f07a82e0ccc07003a4c362bcd000
jaellysbales <jaellysbales@gmail.com>
2015-05-19 20:43:19 -0400

    Making intermediate commit

--- src/main/java/nyc/c4q/AbstractAwesomeClass.java
+++ src/main/java/nyc/c4q/AbstractAwesomeClass.java
@@ -4,14 +4,15 @@ package nyc.c4q;
  * Created by amyquispe on 5/19/15.
  */
 public abstract class AbstractAwesomeClass implements AwesomeInterface {
+    private int data;
 
     @Override
     public int getData() {
-        return 0;
+        return this.data;
     }
 
     @Override
     public void setData(int someData) {
-
+        this.data = someData;
     }
 }
--- src/main/java/nyc/c4q/InitialActivity.java
+++ src/main/java/nyc/c4q/InitialActivity.java
@@ -11,28 +11,53 @@ import android.widget.TextView;
 
 public class InitialActivity extends Activity {
 
-  public int counter = 0;
-  public SharedPreferences preferences = null;
-  public final static String TAG = "C4QTAG";
+    public int counter = 0;
+    public SharedPreferences preferences = null;
+    public final static String TAG = "C4QTAG";
 
-  public void loadState(){
-    Log.d(TAG, "loadState()");
-    counter = preferences.getInt("counter", 0);
-    Log.d(TAG, "loadState(): counter=="+counter);
-  }
+    public void loadState() {
+        Log.d(TAG, "loadState()");
+        counter = preferences.getInt("counter", 0);
+        Log.d(TAG, "loadState(): counter==" + counter);
+    }
 
-  public void saveState(){
-    Log.d(TAG, "saveState()");
-    Log.d(TAG, "saveState(): counter=="+counter);
-    SharedPreferences.Editor editor = preferences.edit();
-    editor.putInt("counter", counter);
-    editor.commit();
-  }
+    public void saveState() {
+        Log.d(TAG, "saveState()");
+        Log.d(TAG, "saveState(): counter==" + counter);
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putInt("counter", counter);
+        editor.commit();
+    }
 
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_initial);
-    preferences = getPreferences(Context.MODE_PRIVATE);
-  }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_initial);
+        preferences = getPreferences(Context.MODE_PRIVATE);
+
+        Button buttonPlus = (Button) findViewById(R.id.buttonPlus);
+        Button buttonMinus = (Button) findViewById(R.id.buttonMinus);
+
+        buttonPlus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                counter++;
+                TextView tvCounter = (TextView) findViewById(R.id.tvCounter);
+                tvCounter.setText(String.valueOf(counter));
+            }
+        });
+
+        buttonMinus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                counter--;
+                TextView tvCounter = (TextView) findViewById(R.id.tvCounter);
+                tvCounter.setText(String.valueOf(counter));
+            }
+        });
+
+
+    }
+
+
 }
