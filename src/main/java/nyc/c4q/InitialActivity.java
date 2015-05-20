358735afaabfc054541a5725974f3d79cc6e24ab
KMaragh <ksmaragh@gmail.com>
2015-05-19 21:12:53 -0400

    Finished assessment

--- src/main/java/nyc/c4q/InitialActivity.java
+++ src/main/java/nyc/c4q/InitialActivity.java
@@ -2,6 +2,7 @@ package nyc.c4q;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
@@ -9,11 +10,14 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class InitialActivity extends Activity {
+public class InitialActivity extends Activity implements View.OnClickListener{
 
   public int counter = 0;
   public SharedPreferences preferences = null;
   public final static String TAG = "C4QTAG";
+  private Button buttonPlus, buttonMinus, buttonTileActivity;
+  private TextView tvCounter;
+
 
   public void loadState(){
     Log.d(TAG, "loadState()");
@@ -34,5 +38,43 @@ public class InitialActivity extends Activity {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_initial);
     preferences = getPreferences(Context.MODE_PRIVATE);
+
+      loadState();
+      buttonPlus = (Button) findViewById(R.id.buttonPlus);
+      buttonMinus = (Button) findViewById(R.id.buttonMinus);
+      buttonTileActivity = (Button) findViewById(R.id.buttonTileActivity);
+      tvCounter = (TextView) findViewById(R.id.tvCounter);
+
+      buttonPlus.setOnClickListener(this);
+      buttonMinus.setOnClickListener(this);
+      buttonTileActivity.setOnClickListener(this);
+
+
+
+      tvCounter.setText(String.valueOf(counter));
+
   }
+
+    @Override
+    public void onClick(View view) {
+        if(view == buttonPlus){
+            counter = Integer.parseInt(tvCounter.getText().toString());
+            counter++;
+            saveState();
+            tvCounter.setText(String.valueOf(counter));
+
+        }
+        else if(view == buttonMinus){
+            counter = Integer.parseInt(tvCounter.getText().toString());
+            counter--;
+            saveState();
+            tvCounter.setText(String.valueOf(counter));
+
+        }
+        else if(view == buttonTileActivity){
+            Intent nextStartedActivity = new Intent(getApplicationContext(), TileActivity.class);
+            startActivity(nextStartedActivity);
+        }
+    }
+
 }
