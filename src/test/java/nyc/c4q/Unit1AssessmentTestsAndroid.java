858b783c93cf68aed783d7ba447dd3c25f99d024
Ehtesh Choudhury <ehtesh.choudhury@gmail.com>
2015-05-26 19:44:00 -0400

    fix layout changes

--- src/test/java/nyc/c4q/Unit1AssessmentTestsAndroid.java
+++ src/test/java/nyc/c4q/Unit1AssessmentTestsAndroid.java
@@ -111,7 +111,7 @@ public class Unit1AssessmentTestsAndroid {
         assertNotNull("TextView(@+id/tvCounter) should not be null", tvCounter);
 
         assertEquals(0, Integer.parseInt((String) tvCounter.getText()));
-        for (int i=1; i<10; i++){
+        for (int i = 1; i < 10; i++) {
             buttonPlus.callOnClick();
             assertEquals(i, Integer.parseInt((String) tvCounter.getText()));
         }
@@ -127,7 +127,7 @@ public class Unit1AssessmentTestsAndroid {
         assertNotNull("TextView(@+id/tvCounter) should not be null", tvCounter);
 
         assertEquals(0, Integer.parseInt((String) tvCounter.getText()));
-        for (int i=-1; i>-10; i--){
+        for (int i = -1; i > -10; i--) {
             buttonMinus.callOnClick();
             assertEquals(i, Integer.parseInt((String) tvCounter.getText()));
         }
@@ -148,12 +148,11 @@ public class Unit1AssessmentTestsAndroid {
 
         assertEquals(0, Integer.parseInt((String) tvCounter.getText()));
         int internalCount = 0;
-        for (int i=0; i<10000; i++){
-            if (r.nextBoolean()){
+        for (int i = 0; i < 10000; i++) {
+            if (r.nextBoolean()) {
                 internalCount++;
                 buttonPlus.callOnClick();
-            }
-            else {
+            } else {
                 internalCount--;
                 buttonMinus.callOnClick();
             }
@@ -162,7 +161,7 @@ public class Unit1AssessmentTestsAndroid {
     }
 
     @Test
-    public void test05TvCounterStateShouldBeSavedThroughActivityLifeCycle(){
+    public void test05TvCounterStateShouldBeSavedThroughActivityLifeCycle() {
         // use InitialActivity.saveState() and InitialActivity.loadState() methods
         ActivityController<InitialActivity> controller = Robolectric.buildActivity(InitialActivity.class).setup();
         InitialActivity activity = controller.get();
@@ -210,55 +209,52 @@ public class Unit1AssessmentTestsAndroid {
         TileActivity ta = Robolectric.buildActivity(TileActivity.class).setup().get();
         LinearLayout taLayout = (LinearLayout) ta.findViewById(R.id.activity_tile);
 
-        assertTrue("LinearLayout(@+id/activity_tile) should have a horizontal orientation",
-                taLayout.getOrientation() == LinearLayout.HORIZONTAL);
+        assertTrue("LinearLayout(@+id/activity_tile) should have a horizontal orientation", taLayout.getOrientation() == LinearLayout.HORIZONTAL);
 
         assertTrue("LinearLayout(@+id/activity_tile)[0] should be a LinearLayout", taLayout.getChildAt(0) instanceof LinearLayout);
         LinearLayout leftSide = (LinearLayout) taLayout.getChildAt(0);
         assertTrue("LinearLayout(@+id/activity_tile)[0] should have R.id.leftSide", Helpers.getResourceId(leftSide).equals("leftSide"));
         assertTrue("LinearLayout(@+id/activity_tile)[0] should be equal to LinearLayout(@+id/leftSide)", leftSide == Helpers.findViewByIdString(ta, "leftSide"));
-        assertTrue("LinearLayout(@+id/activity_tile)[0] should have a vertical orientation",
-                leftSide.getOrientation() == LinearLayout.VERTICAL);
+        assertTrue("LinearLayout(@+id/activity_tile)[0] should have a vertical orientation", leftSide.getOrientation() == LinearLayout.VERTICAL);
         assertEquals("View(@+id/leftSide) should have layout_weight='1'", 1, ((LinearLayout.LayoutParams) leftSide.getLayoutParams()).weight, 0.01);
 
         assertTrue("LinearLayout(@+id/activity_tile)[1] should be a LinearLayout", taLayout.getChildAt(1) instanceof LinearLayout);
         LinearLayout rightSide = (LinearLayout) taLayout.getChildAt(1);
         assertTrue("LinearLayout(@+id/activity_tile)[1] should have R.id.rightSide", Helpers.getResourceId(rightSide).equals("rightSide"));
         assertTrue("LinearLayout(@+id/activity_tile)[1] should be equal to LinearLayout(@+id/rightSide)", rightSide == Helpers.findViewByIdString(ta, "rightSide"));
-        assertTrue("LinearLayout(@+id/activity_tile)[1] should have a vertical orientation",
-                rightSide.getOrientation() == LinearLayout.VERTICAL);
+        assertTrue("LinearLayout(@+id/activity_tile)[1] should have a vertical orientation", rightSide.getOrientation() == LinearLayout.VERTICAL);
         assertEquals("View(@+id/rightSide) should have layout_weight='1'", 1, ((LinearLayout.LayoutParams) rightSide.getLayoutParams()).weight, 0.01);
 
 
-        View redView  = leftSide.getChildAt(0);
+        View redView = leftSide.getChildAt(0);
         assertTrue("LinearLayout(@+id/leftSide)[0] should have R.id.redView", Helpers.getResourceId(redView).equals("redView"));
         assertTrue("LinearLayout(@+id/leftSide)[0] should be equal to View(@+id/redView)", redView == Helpers.findViewByIdString(ta, "redView"));
         Drawable redViewBackground = redView.getBackground();
         assertThat(redViewBackground, instanceOf(ColorDrawable.class));
         assertEquals(0xffff0000, ((ColorDrawable) redViewBackground).getColor());
 
-        View greenView  = leftSide.getChildAt(1);
+        View greenView = leftSide.getChildAt(1);
         assertTrue("LinearLayout(@+id/leftSide)[1] should have R.id.greenView", Helpers.getResourceId(greenView).equals("greenView"));
         assertTrue("LinearLayout(@+id/leftSide)[1] should be equal to View(@+id/greenView)", greenView == Helpers.findViewByIdString(ta, "greenView"));
         Drawable greenViewBackground = greenView.getBackground();
         assertThat(greenViewBackground, instanceOf(ColorDrawable.class));
         assertEquals(0xff00ff00, ((ColorDrawable) greenViewBackground).getColor());
 
-        View yellowView  = rightSide.getChildAt(0);
+        View yellowView = rightSide.getChildAt(0);
         assertTrue("LinearLayout(@+id/rightSide)[0] should have R.id.yellowView", Helpers.getResourceId(yellowView).equals("yellowView"));
         assertTrue("LinearLayout(@+id/rightSide)[0] should be equal to View(@+id/yellowView)", yellowView == Helpers.findViewByIdString(ta, "yellowView"));
         Drawable yellowViewBackground = yellowView.getBackground();
         assertThat(yellowViewBackground, instanceOf(ColorDrawable.class));
         assertEquals(0xffffff00, ((ColorDrawable) yellowViewBackground).getColor());
 
-        View whiteView  = rightSide.getChildAt(1);
+        View whiteView = rightSide.getChildAt(1);
         assertTrue("LinearLayout(@+id/rightSide)[1] should have R.id.whiteView", Helpers.getResourceId(whiteView).equals("whiteView"));
         assertTrue("LinearLayout(@+id/rightSide)[1] should be equal to View(@+id/whiteView)", whiteView == Helpers.findViewByIdString(ta, "whiteView"));
         Drawable whiteViewBackground = whiteView.getBackground();
         assertThat(whiteViewBackground, instanceOf(ColorDrawable.class));
         assertEquals(0xffffffff, ((ColorDrawable) whiteViewBackground).getColor());
 
-        View blueView  = rightSide.getChildAt(2);
+        View blueView = rightSide.getChildAt(2);
         assertTrue("LinearLayout(@+id/rightSide)[2] should have R.id.blueView", Helpers.getResourceId(blueView).equals("blueView"));
         assertTrue("LinearLayout(@+id/rightSide)[2] should be equal to View(@+id/blueView)", blueView == Helpers.findViewByIdString(ta, "blueView"));
         Drawable blueViewBackground = blueView.getBackground();
